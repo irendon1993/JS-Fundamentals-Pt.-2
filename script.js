734617f551e7e885d2dd1afea78c76a0ef8a88a8
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-12 11:28:26 -0500

    Practicing functions

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-"use strict";
+// "use strict";
 
-let hasDriversLiscenese = false;
-const passtest = true;
+// let hasDriversLiscenese = false;
+// const passtest = true;
 
-if (passtest) hasDriversLiscenese = true;
-if (hasDriversLiscenese) console.log("I can drive");
+// if (passtest) hasDriversLiscenese = true;
+// if (hasDriversLiscenese) console.log("I can drive");
+//
+//
+//
+//
+//
+//
+//
+//
+function logger() {
+  console.log("My name is Isaac");
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProccessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProccessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProccessor(2, 4);
+console.log(appleOrangeJuice);
