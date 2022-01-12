632a8b4c35905fccf7fc21a01eb24a2f1b830601
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-12 12:01:34 -0500

    Arrow functions

--- script.js
+++ script.js
@@ -39,19 +39,34 @@
 //
 //
 //
-// Function Declaration
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
+// // Function Declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
 
-const age1 = calcAge1(1993);
-console.log(age1);
+// const age1 = calcAge1(1993);
+// console.log(age1);
 
-// Function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1993);
+//
+//
+// Arrow function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1993);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires is ${retirement} years`;
 };
 
-const age2 = calcAge2(1993);
-
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1993, "Isaac"));
+console.log(yearsUntilRetirement(1977, "Bill"));
