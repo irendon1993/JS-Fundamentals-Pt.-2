a01e80e924dd851cde2582e1fc828998d744cb20
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-13 17:54:30 -0500

    While loops

--- script.js
+++ script.js
@@ -523,6 +523,15 @@ GOOD LUCK 😀
 
 let rep = 1;
 while (rep <= 10) {
-  console.log(`Lifting Weight repetition ${rep}`);
+  // console.log(`Lifting Weight repetition ${rep}`);
   rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log(`Loop is about to end...`);
 }
