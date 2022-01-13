80e50ee9ecbc26ba2b5462a53d56e8cc1c6086ba
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-13 10:44:55 -0500

    Arrayyyyyyys

--- script.js
+++ script.js
@@ -162,5 +162,35 @@ const friend3 = "Jesus";
 const friends = ["Trey", "Kevin", "Jesus"];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
+const y = new Array(1991, 1984, 2008, 2020);
 console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Isaac";
+const isaac = [firstName, "Rendon", 2037 - 1993, "student", friends];
+console.log(isaac);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
